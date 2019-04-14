7343ab4e49173c9e5b4de1e7f470dbb6d9391fe5
hilimps <hilimps@gmail.com>
2019-04-14 18:47:15 +0900

    요청테스트

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -76,7 +76,9 @@ public class Owner extends Person {
     private String email;
 
     public String getEmail( ) {
+    	
     	return email;
+    	
     }
     
     public void setEmail(String email) {
